8d6d9f69b691887bb8f5ce7330e8b73741c5a3eb
Alfonso <alfonsojunior@gmail.com>
2018-05-04 06:59:09 -0300

    Separação das tabelas

--- app/src/main/java/br/edu/unidavi/unidavijava/data/DatabaseHelper.java
+++ app/src/main/java/br/edu/unidavi/unidavijava/data/DatabaseHelper.java
@@ -6,8 +6,18 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,16 +27,21 @@ import br.edu.unidavi.unidavijava.model.MeuJogo;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "Games.db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 3;
     private static final String TABLE_GAME = "GAME";
+    private static final String TABLE_MYGAME = "MYGAME";
+    private Context ctx;
 
     public DatabaseHelper (Context context) {
 
         super(context, DB_NAME, null, DB_VERSION);
+        this.ctx = context;
+        //Log.v("DatabaseHelper_CONSTRUT",Environment.getDataDirectory() + "/data/br.edu.unidavi.unidavijava/databases/" + DB_NAME);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Log.v("DatabaseHelper_CREATE", "Criação da database");
         String CREATE_TABLE = "CREATE TABLE "
                 + TABLE_GAME + "("
                 + "codigo INTEGER PRIMARY KEY AUTOINCREMENT,"
@@ -45,12 +60,120 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + "notaPessoal REAL DEFAULT 0.0," +
                 "CONSTRAINT jogo_unico UNIQUE (nome, plataforma)"
                 + ")";
-        sqLiteDatabase.execSQL(CREATE_TABLE);
+        try {
+            sqLiteDatabase.execSQL(CREATE_TABLE);
+        } catch (Exception e) { }
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        Log.v("DB_UPGRADE", "Nada para fazer");
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        //Log.v("DB_UPGRADE", "Nada para fazer");
+        Log.v("DatabaseHelper_UPDATE", "Atualização da database");
+        Log.v("DatabaseHelper_UPDATE", oldVersion + " < " + newVersion);
+        // Eliminação da tabela que já existia
+        if (newVersion <= 3) {
+            String CREATE_TABLE = "DROP TABLE IF EXISTS " + TABLE_GAME;
+            try {
+                sqLiteDatabase.execSQL(CREATE_TABLE);
+            } catch (Exception e) {
+            }
+
+            CREATE_TABLE = "CREATE TABLE "
+                    + TABLE_GAME + "("
+                    + "codigo INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "nome TEXT,"
+                    + "plataforma TEXT,"
+                    + "lancamento INTEGER, "
+                    + "genero TEXT,"
+                    + "imageUrl TEXT,"
+                    + "nota REAL,"
+                    + "CONSTRAINT jogo_unico UNIQUE (nome, plataforma)"
+                    + ")";
+            try {
+                sqLiteDatabase.execSQL(CREATE_TABLE);
+            } catch (Exception e) {
+            }
+
+            CREATE_TABLE = "CREATE TABLE "
+                    + TABLE_MYGAME + "("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "codigo_game INTEGER,"
+                    + "quero INTEGER DEFAULT 0,"
+                    + "tenho INTEGER DEFAULT 0,"
+                    + "joguei INTEGER DEFAULT 0,"
+                    + "zerei INTEGER DEFAULT 0,"
+                    + "fisico INTEGER DEFAULT 0,"
+                    + "paguei REAL DEFAULT 0.0,"
+                    + "notaPessoal REAL DEFAULT 0.0,"
+                    + "CONSTRAINT meujogo_unico UNIQUE (codigo_game)"
+                    + ")";
+            try {
+                sqLiteDatabase.execSQL(CREATE_TABLE);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public void exportDB(){
+        File sd = Environment.getExternalStorageDirectory();
+        File data = Environment.getDataDirectory();
+        FileChannel source=null;
+        FileChannel destination=null;
+        String currentDBPath = this.ctx.getDatabasePath(DB_NAME).toString();
+        Log.v("DATABASE", currentDBPath);
+        String backupDBPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + DB_NAME + ".bak";
+        //String backupDBPath = "/sdcard/Download/" + DB_NAME + ".bak";
+
+        Log.v("BACKUP", backupDBPath);
+        File currentDB = new File(currentDBPath);
+        File backupDB = new File(backupDBPath);
+        boolean success = backupDB.mkdirs();
+        try {
+            source = new FileInputStream(currentDB).getChannel();
+            destination = new FileOutputStream(backupDB).getChannel();
+            destination.transferFrom(source, 0, source.size());
+            source.close();
+            destination.close();
+            Toast.makeText(this.ctx, "DB Exported!", Toast.LENGTH_LONG).show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void backupDB() {
+        final String inFileName = this.ctx.getDatabasePath(DB_NAME).toString();
+        Log.v("DATABASE", inFileName);
+        //final String outFileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/br.edu.unidavi.unidavijava/" + DB_NAME;
+        final String outFileName = "/sdcard/Download" + "/br.edu.unidavi.unidavijava/" + DB_NAME;
+        Log.v("BACKUP", outFileName);
+
+        try {
+
+            File dbFile = new File(inFileName);
+            FileInputStream fis = new FileInputStream(dbFile);
+
+            // Open the empty db as the output stream
+            boolean success = new File("/sdcard/Download" + "/br.edu.unidavi.unidavijava/").mkdirs();
+            OutputStream output = new FileOutputStream(outFileName);
+
+            // Transfer bytes from the input file to the output file
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+
+            // Close the streams
+            output.flush();
+            output.close();
+            fis.close();
+
+            Toast.makeText(this.ctx, "Backup Criado", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Toast.makeText(this.ctx, "Unable to backup database. Retry", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     public void createJogo(Jogo jogo) {
@@ -64,7 +187,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put("imageUrl", jogo.getImageUrl());
         values.put("nota", jogo.getNota());
 
-        //db.insert(TABLE_GAME, null, values);
         try {
             db.insertOrThrow(TABLE_GAME, null, values);
         } catch (SQLException e) {
@@ -76,20 +198,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
-    public int createMeuJogo(MeuJogo jogo) {
+    public long createMeuJogo(MeuJogo jogo) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
         //values.put("nome", jogo.getNome());
         //values.put("plataforma", jogo.getPlataforma());
-        if (jogo.getLancamento() != 0)
-            values.put("lancamento", jogo.getLancamento());
-        if (!jogo.getGenero().equals(""))
-            values.put("genero", jogo.getGenero());
-        if (!jogo.getImageUrl().equals(""))
-            values.put("imageUrl", jogo.getImageUrl());
-        if (jogo.getNota() != 0)
-            values.put("nota", jogo.getNota());
+        values.put("codigo_game", jogo.getId());
         values.put("quero", jogo.isQuero());
         values.put("tenho", jogo.isTenho());
         values.put("joguei", jogo.isJoguei());
@@ -98,8 +213,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put("paguei", jogo.getPaguei());
         values.put("notaPessoal", jogo.getNotaPessoal());
 
-        int ret = db.update(TABLE_GAME, values, "nome = '" + jogo.getNome().replace("'", "''").trim() + "' AND plataforma = '"+jogo.getPlataforma().replace("'", "''").trim()+"'", null);
-        db.close();
+        //int ret = db.update(TABLE_GAME, values, "nome = '" + jogo.getNome().replace("'", "''").trim() + "' AND plataforma = '"+jogo.getPlataforma().replace("'", "''").trim()+"'", null);
+        //db.close();
+        long ret = 0;
+        try {
+            ret = db.insertOrThrow(TABLE_MYGAME, null, values);
+        } catch (SQLException e) {
+            ret = db.update(TABLE_MYGAME, values, "codigo_game = " + jogo.getId(), null);
+        } finally {
+            if (db != null) {
+                db.close();
+            }
+        }
         return ret;
     }
 
@@ -196,8 +321,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         List<MeuJogo> jogoList = new ArrayList<MeuJogo>();
 
         String ordenacao = getOrdem(order);
-        String selectQuery = "SELECT * FROM " + TABLE_GAME
-                + " WHERE quero = 1 OR tenho = 1 OR joguei = 1 OR zerei = 1 OR fisico = 1 "
+        String selectQuery = "SELECT a.codigo_game, b.nome, b.plataforma, b.lancamento, b.genero, "
+                + " b.imageUrl, b.nota, a.quero, a.tenho, a.joguei, a.zerei, a.fisico, a.paguei, "
+                + " a.notaPessoal FROM " + TABLE_MYGAME + " AS a "
+                + " INNER JOIN " + TABLE_GAME + " AS b "
+                + " ON a.codigo_game = b.codigo "
                 + ordenacao;
 
         SQLiteDatabase db = this.getReadableDatabase();
@@ -247,8 +375,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public List<MeuJogo> getMeuJogo(Integer id) {
         List<MeuJogo> jogoList = new ArrayList<MeuJogo>();
 
-        String selectQuery = "SELECT * FROM " + TABLE_GAME
-                + " WHERE codigo = "
+        String selectQuery = "SELECT a.codigo_game, b.nome, b.plataforma, b.lancamento, b.genero, "
+                + " b.imageUrl, b.nota, a.quero, a.tenho, a.joguei, a.zerei, a.fisico, a.paguei, "
+                + " a.notaPessoal FROM " + TABLE_MYGAME + " AS a "
+                + " INNER JOIN " + TABLE_GAME + " AS b "
+                + " ON a.codigo_game = b.codigo "
+                + " WHERE a.codigo_game = "
                 + id.toString();
 
         SQLiteDatabase db = this.getReadableDatabase();
@@ -298,7 +430,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public List<Float> getInfoRanking(){
         List<Float> ranking = new ArrayList<Float>();
 
-        String selectQuery = "SELECT sum(tenho) as tenho, sum(joguei) as joguei, sum(zerei) as zerei, sum(quero) as quero, sum(paguei) as paguei FROM " + TABLE_GAME;
+        String selectQuery = "SELECT sum(tenho) as tenho, sum(joguei) as joguei, sum(zerei) as zerei, sum(quero) as quero, sum(paguei) as paguei FROM " + TABLE_MYGAME;
 
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(selectQuery, null);
